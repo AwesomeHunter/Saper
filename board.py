00ad91977fab487118262c6ee2bfde92a09f8206
Adam Ciezkowski <adamciezkowski19@gmail.com>
2021-04-22 18:58:29 +0200

    Refactored code

--- board.py
+++ board.py
@@ -1,35 +1,38 @@
-from vector import Vector
+from pygame.math import Vector2
 from random import random
 from tile_type import TileType
 
 
 class Board:
-    neighbours = [Vector(neighbour) for neighbour in
+    neighbours = [Vector2(neighbour) for neighbour in
                   [(1, 1), (1, 0), (0, 1), (-1, -1), (-1, 0), (0, -1), (-1, 1), (1, -1)]]
 
     def __init__(self, size):
-        self.SIZE = self.WIDTH, self.HEIGHT = size.tup()
-        self.board = {Vector(x, y): TileType.EM_HID for x in range(self.WIDTH) for y in range(self.HEIGHT)}
+        self.size = size
+        self.board = {(x, y): TileType.EM_HID for x in range(int(self.size.x)) for y in range(int(self.size.y))}
 
     def count_tiles(self, *args):
         return sum([1 for key, val in self.board.items() if val in args])
-
+    
+    def gen_rand_position(self):
+        x = int(random() * self.size.x)
+        y = int(random() * self.size.y)
+        return Vector2(x, y)
+        
     def gen_bombs(self, number_of_bombs, mouse_pos):
         not_allowed_positions = [mouse_pos + neighbour for neighbour in self.neighbours] + [mouse_pos]
         while self.count_tiles(TileType.BO_HID) < number_of_bombs:
-            x = int(random() * self.WIDTH)
-            y = int(random() * self.HEIGHT)
-            random_pos = Vector(x, y)
+            random_pos = self.gen_rand_position()
             if random_pos not in not_allowed_positions:
-                self.board[random_pos] = TileType.BO_HID
+                self.board[tuple(random_pos)] = TileType.BO_HID
 
     def count_bombs_at_pos(self, pos):
         return sum([1 for neighbour in self.neighbours
-                    if self.board.get(pos + neighbour) in [TileType.BO_HID, TileType.BO_FLA, TileType.BO_VIS]])
+                    if self.board.get(tuple(pos + neighbour)) in [TileType.BO_HID, TileType.BO_FLA, TileType.BO_VIS]])
 
     def clicked_empty_tile(self, pos):
-        if self.board.get(pos) in [TileType.EM_HID, TileType.EM_FLA]:
-            self.board[pos] = TileType.EM_VIS
+        if self.board.get(tuple(pos)) in [TileType.EM_HID, TileType.EM_FLA]:
+            self.board[tuple(pos)] = TileType.EM_VIS
             if self.count_bombs_at_pos(pos) == 0:
                 for neighbour in self.neighbours:
                     self.clicked_empty_tile(neighbour + pos)
@@ -38,7 +41,7 @@ class Board:
         return self.board
 
     def get_type(self, pos):
-        return self.board.get(pos)
+        return self.board.get(tuple(pos))
 
     def flag_action(self, pos):
         flag_tile_type = {
@@ -47,9 +50,9 @@ class Board:
             TileType.EM_HID: TileType.EM_FLA,
             TileType.EM_FLA: TileType.EM_HID,
         }
-        self.board[pos] = flag_tile_type.get(self.get_type(pos), self.get_type(pos))
+        self.board[tuple(pos)] = flag_tile_type.get(self.get_type(pos), self.get_type(pos))
 
     def replace_type(self, type_to_replace, new_type):
         for pos, tile_type in self.board.items():
             if tile_type == type_to_replace:
-                self.board[pos] = new_type
+                self.board[tuple(pos)] = new_type
